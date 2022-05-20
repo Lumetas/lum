080e4354e6a25a80416a8e71246197291f6922a1
110000110000 <98038223+110000110000@users.noreply.github.com>
2022-05-20 15:17:33 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-sudo rm /lum.py; sudo rm /usr/bin/lum;
+sudo rm /usr/bin/lum;
 sudo chmod +x lum;
-sudo chmod +x lum.py;
 sudo mv lum /usr/bin;
-sudo mv lum.py /;
 sudo mkdir /lum
 sudo mkdir /lumRm;
 pip install -r libary.txt;
